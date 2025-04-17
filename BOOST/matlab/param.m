6570787c98f551556b96cc96660f62c65a75199e
marcos2307 <gomezredondomarcos@gmail.com>
2025-04-17 17:51:13 -0400

    First results of microgrid with single phase ac. Files used, plotting script and .mat are added to show the results. Voltage regulation for MPPT is now in a closed loop. VSI current is controlled by a PR.

--- BOOST/matlab/param.m
+++ BOOST/matlab/param.m
@@ -1,18 +1,15 @@
 %%parameters boost
 
-T = 10e-3;
+T = 50e-6;
 
 fixedgain_v1 = 10*44000/(3*100*2.5);
 gain_v1 = 1;
-offset_v1 = 0;
+offset_v1 = -10;
 
 fixedgain_i1 = 10*1000/(3*100);
 gain_i1 = 1;
-offset_i1 = 0;
+offset_i1 = .1;
 
 fixedgain_v2 = 10*10000/(3*100*2.5);
 gain_v2 = 1;
 offset_v2 = 0;
-
-num = 1/500*ones(1,500);
-den = [1, zeros(1,499)];
